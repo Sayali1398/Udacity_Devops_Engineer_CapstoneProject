53ef94993c199d2cfad85b9b788ef34990388f03
EC2 Default User <ec2-user@ip-172-31-73-210.ec2.internal>
2023-03-05 07:28:21 +0000

    passed lint test 1

--- app.py
+++ app.py
@@ -4,6 +4,6 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
-        return "Congratulations, it's a web app!"
+    return "Congratulations, it's a web app!"
 if __name__ == "__main__":
-        app.run(host="0.0.0.0", port=8080, debug=True)
+    app.run(host="0.0.0.0", port=8080, debug=True)
